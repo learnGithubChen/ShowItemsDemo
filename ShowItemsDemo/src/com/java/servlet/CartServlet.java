d11f0ee87d7b5835f5132264eb59adb8614cadd3
learnGithubChen <729245768@qq.com>
2016-10-07 16:51:15 +0800

    添加CartServlet类用来处理jsp页面传来有关购物车相关请求

---
+++ ShowItemsDemo/src/com/java/servlet/CartServlet.java
@@ -0,0 +1,24 @@
+package com.java.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CartServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/success.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
